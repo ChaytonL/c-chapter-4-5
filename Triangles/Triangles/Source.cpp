53dde044a3024ed5aa0753f759a10ce7e61bd26e
ChaytonL <chaytonlofgren@gmail.com>
2018-09-18 11:12:46 -0500

    Triangles

--- Triangles/Triangles/Source.cpp
+++ Triangles/Triangles/Source.cpp
@@ -6,40 +6,166 @@ using namespace std;
 
 int rightTriangle() {
 
-	float sideA;
-	float sideB;
-	float sideC;
-	float area;
-	float perimeter;
+	double sideA;
+	double sideB;
+	double sideC;
+	double area;
+	double perimeter;
+	int userchoice;
 
-	cout << "----------------------------------" << endl;
-	cout << " What would you like side A to be? >> " << endl;
-	cin >> sideA;
-	cout << "----------------------------------" << endl;
-	cout << " What would you like side B to be? >> " << endl;
-	cin >> sideB;
+	cout << " What side are you tying to find\n 1=A\n 2=B\n 3=c\n" << endl;
+	cin >> userchoice;
 
-	sideC = sqrt((sideA*sideA) + (sideB*sideB));
-	area = .5 *sideA*sideB;
-	perimeter = sideC + sideB + sideA;
+	if (userchoice == 1) {
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side B to be? >> " << endl;
+		cin >> sideB;
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side C to be? >> " << endl;
+		cin >> sideC;
+
+		if (sideC < sideB) {
+			cout << " Not available ";
+			rightTriangle();
+		}
+		else {
+			sideA = sqrt((sideC*sideC) - (sideB*sideB));
+			area = .5 *sideA*sideB;
+			perimeter = sideC + sideB + sideA;
+
+			cout << fixed << setprecision(2) << " The missing side is " << sideA << endl;
+			cout << fixed << setprecision(2) << " The area is " << area << endl;
+			cout << fixed << setprecision(2) << " The perimeter is " << perimeter << endl;
+		}
+	}
+	else if (userchoice == 2) {
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side A to be? >> " << endl;
+		cin >> sideA;
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side C to be? >> " << endl;
+		cin >> sideC;
+
+		if (sideC < sideA) {
+			cout << " Not available ";
+			rightTriangle();
+		}
+		else {
+			sideB = sqrt((sideC*sideC) - (sideA*sideA));
+			area = .5 *sideA*sideB;
+			perimeter = sideC + sideB + sideA;
+
+			cout << fixed << setprecision(2) << " The missing side is " << sideB << endl;
+			cout << fixed << setprecision(2) << " The area is " << area << endl;
+			cout << fixed << setprecision(2) << " The perimeter is " << perimeter << endl;
+		}
+	}
+	else if (userchoice == 3) {
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side A to be? >> " << endl;
+		cin >> sideA;
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side B to be? >> " << endl;
+		cin >> sideB;
+
+		sideC = sqrt((sideA*sideA) + (sideB*sideB));
+		area = .5 *sideA*sideB;
+		perimeter = sideC + sideB + sideA;
+
+		cout << fixed << setprecision(2) << " The missing side is " << sideC << endl;
+		cout << fixed << setprecision(2) << " The area is " << area << endl;
+		cout << fixed << setprecision(2) << " The perimeter is " << perimeter << endl;
+
+	}
 	
-	cout << fixed << setprecision(2) << " The missing side is " << sideC << endl;
-	cout << fixed << setprecision(2) << " The area is " << area << endl;
-	cout << fixed << setprecision(2) << " The perimeter is " << perimeter << endl;
-
-	
-	system("pause");
 	return 0;
 }
 
 
 int isoceles() {
 
+	double sideA = 0.0;
+	double sideB = 0.0;
+	double sideC = 0.0;
+	double area;
+	double perimeter;
+	double height;
+	int userchoice;
+
+	cout << " What side are you tying to find\n 1=A\n 2=c\n" << endl;
+	cin >> userchoice;
+	if (userchoice == 1) {
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side B to be? >> " << endl;
+		cin >> sideB;
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side C to be? >> " << endl;
+		cin >> sideC;
+		cout << " What is the height? " << endl;
+		cin >> height;
+
+
+		if (sideC < sideA) {
+			cout << " Not available ";
+			rightTriangle();
+		}
+		else {
+			sideC = sideA;
+			area = (sideB*height) / 2;
+			perimeter = (sideA*sideA) + sideB;
+
+			cout << fixed << setprecision(2) << " The missing side is " << sideA << endl;
+			cout << fixed << setprecision(2) << " The area is " << area << endl;
+			cout << fixed << setprecision(2) << " The perimeter is " << perimeter << endl;
+		}
+	}
+	else if (userchoice == 2) {
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side A to be? >> " << endl;
+		cin >> sideA;
+		cout << "----------------------------------" << endl;
+		cout << " What would you like side B to be? >> " << endl;
+		cin >> sideB;
+		cout << " What is the height? " << endl;
+		cin >> height;
+
+		sideC = sideA;
+		area = (sideB*height) / 2;
+		perimeter = (sideA*sideA) + sideB;
+
+		cout << fixed << setprecision(2) << " The missing side is " << sideC << endl;
+		cout << fixed << setprecision(2) << " The area is " << area << endl;
+		cout << fixed << setprecision(2) << " The perimeter is " << perimeter << endl;
+
+	}
+
+	system("pause");
 	return 0;
 }
 
 int equilateral() {
 
+	double sideA;
+	double sideB;
+	double sideC;
+	double area;
+	double perimeter;
+
+	cout << "----------------------------------" << endl;
+	cout << " What would you like side A to be? >> " << endl;
+	cout << "----------------------------------" << endl;
+	cin >> sideA;
+
+	sideB = sideA;
+	sideC = sideA;
+	area = (sqrt(3) / 4) * pow(sideA, 2);
+	perimeter = sideC + sideB + sideA;
+
+	cout << fixed << setprecision(2) << " The other sides are " << sideB << endl;
+	cout << fixed << setprecision(2) << " The area is " << area << endl;
+	cout << fixed << setprecision(2) << " The perimeter is " << perimeter << endl;
+
+	system("pause");
 	return 0;
 }
 
